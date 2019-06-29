46904709b5394854e8c77e21d72d71c1d31d3ed6
Ryan Turner <ryan@turnrye.com>
2019-06-29 16:28:54 -0500

    Initial template

---
+++ android/app/src/main/java/com/memphis_womens_legacy_trail/MainActivity.java
@@ -0,0 +1,15 @@
+package com.memphis_womens_legacy_trail;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "memphis_womens_legacy_trail";
+    }
+}
