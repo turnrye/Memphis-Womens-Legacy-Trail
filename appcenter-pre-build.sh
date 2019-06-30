2a35d07cc720bd82ebc9f214f9a2ad785936959b
Ryan Turner <ryan@turnrye.com>
2019-06-29 20:40:00 -0500

    Fix issue with missing directory on build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,2 +1,3 @@
 echo "Updating android secret"
+mkdir -p "$APPCENTER_SOURCE_DIRECTORY/android/app/src/main/assets/"
 echo $appcenter_config | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/android/app/src/main/assets/appcenter-config.json"
